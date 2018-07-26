b1a293c4dbcec755e0f09d81930894bc35e35cef
Linklgq <2943510548@qq.com>
2018-07-26 23:55:07 +0800

    数据库的相关配置

---
+++ app/src/main/java/com/example/lenovo/note/db/DBUtil.java
@@ -0,0 +1,98 @@
+package com.example.lenovo.note.db;
+
+import android.widget.Toast;
+
+import com.example.lenovo.note.MyApplication;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.lenovo.note.db.DBUtil.SortType.BY_MODIFIED_TIME;
+
+/**
+ * Created by Lenovo on 2018/7/26.
+ */
+
+public class DBUtil {
+//    public static final int BY_CREATED_TIME=0;
+//    public static final int BY_MODIFIED_TIME=1;
+//    public static final int BY_CONTENT=2;
+    public enum SortType{
+        BY_CREATED_TIME("createdTime"),
+        BY_MODIFIED_TIME("modifiedTime"),
+        BY_CONTENT("content");
+
+        String type;
+
+        SortType(String type){
+            this.type=type;
+        }
+
+        @Override
+        public String toString() {
+            return type;
+        }
+    }
+
+    private static List<Note> noteList=new ArrayList<>();
+    private static final int SIZE=64;
+    private static int start=0;
+    private static SortType sortType= BY_MODIFIED_TIME;
+
+    public static Note query(int index){
+        if(index<start||index>=start+ noteList.size()){
+            start= index-SIZE/2>0?index-SIZE/2:0;
+            noteList = DataSupport.order(sortType+"desc").limit(SIZE).offset(start).find(Note.class);
+        }
+        return noteList.get(index-start);
+    }
+
+    public static void modify(int index){
+        if(index<start||index>=start+ noteList.size()){
+            Toast.makeText(MyApplication.getContext(), "imposible out of index",
+                    Toast.LENGTH_SHORT).show();
+        }else{
+            if(!noteList.get(index).getContent().trim().isEmpty()){
+                remove(noteList.get(index).getId());
+                Toast.makeText(MyApplication.getContext(),
+                        "空便签已自动删除", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            noteList.get(index).save();
+            // TODO: 2018/7/26 滚动到修改便签
+            if(sortType!=SortType.BY_CREATED_TIME){
+                syncData();
+            }
+        }
+    }
+
+    public static void add(Note note){
+        if(!note.getContent().trim().isEmpty()){
+            note.save();
+            // TODO: 2018/7/26  滚动到新便签
+            syncData();
+        }else{
+            Toast.makeText(MyApplication.getContext(),
+                    "空便签将不会保存", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void remove(int id){
+        // TODO: 2018/7/26 回收站
+        DataSupport.where("id=?",String.valueOf(id)).find(Note.class).get(0).delete();
+    }
+
+    public static void setSortType(SortType sortType) {
+        DBUtil.sortType = sortType;
+    }
+
+    public static int getCounts(){
+        return DataSupport.count(Note.class);
+    }
+
+    private static void syncData(){
+        noteList = DataSupport.order(sortType+"desc").limit(SIZE).offset(start).find(Note.class);
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/note/db/Note.java
@@ -0,0 +1,46 @@
+package com.example.lenovo.note.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Lenovo on 2018/7/26.
+ */
+
+public class Note extends DataSupport {
+    private int id;
+    private long createdTime;
+    private long modifiedTime;
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public long getCreatedTime() {
+        return createdTime;
+    }
+
+    public long getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCreatedTime(long createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public void setModifiedTime(long modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
